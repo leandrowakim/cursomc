840229e3311c15325b62ba701f7203fcaa1642c0
Leandro Wakim <leandrowakim@gmail.com>
2020-08-13 15:00:23 -0300

    Inclusão do banco de dados H2 e criação automática da base de dados

--- src/main/java/com/lwinfo/cursomc/domain/Categoria.java
+++ src/main/java/com/lwinfo/cursomc/domain/Categoria.java
@@ -2,10 +2,20 @@ package com.lwinfo.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column(length=60)
 	private String nome;
 	
 	public Categoria() {		
