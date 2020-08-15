4b244b8bacfa9ab66bdd4a1fcb5c4923a0a02bb7
Leandro Wakim <leandrowakim@gmail.com>
2020-08-15 16:31:46 -0300

    Estado e cidade

---
+++ src/main/java/com/lwinfo/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.lwinfo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lwinfo.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
