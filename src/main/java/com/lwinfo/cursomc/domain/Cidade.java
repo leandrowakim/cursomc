c75304655b2497d7424cdc9c28f4248a7d236657
Leandro Wakim <leandrowakim@gmail.com>
2020-08-16 15:11:52 -0300

    Endpoint /clientes/{id} disponível

--- src/main/java/com/lwinfo/cursomc/domain/Cidade.java
+++ src/main/java/com/lwinfo/cursomc/domain/Cidade.java
@@ -10,6 +10,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Cidade implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -20,6 +23,7 @@ public class Cidade implements Serializable {
 	@Column(length=60)
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
