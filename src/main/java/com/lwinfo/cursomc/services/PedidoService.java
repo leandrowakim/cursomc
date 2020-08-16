011f1a59bebd1e591b17ee3d33fe1b01f785be34
Leandro Wakim <leandrowakim@gmail.com>
2020-08-16 17:48:44 -0300

    Endpoint /pedidos/{id} disponível

---
+++ src/main/java/com/lwinfo/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.lwinfo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lwinfo.cursomc.domain.Pedido;
+import com.lwinfo.cursomc.repositories.PedidoRepository;
+import com.lwinfo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));		
+	}
+}
