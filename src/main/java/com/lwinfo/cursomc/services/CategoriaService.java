ed3ce3191d9e5a40b411b5f53aa55afe3e74b183
Leandro Wakim <leandrowakim@gmail.com>
2020-08-17 21:02:12 -0300

    Listando todas categorias com DTO

--- src/main/java/com/lwinfo/cursomc/services/CategoriaService.java
+++ src/main/java/com/lwinfo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.lwinfo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.lwinfo.cursomc.domain.Categoria;
 import com.lwinfo.cursomc.repositories.CategoriaRepository;
-import com.lwinfo.cursomc.services.exceptions.DataIntegrityException; 
+import com.lwinfo.cursomc.services.exceptions.DataIntegrityException;
 import com.lwinfo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -41,5 +42,9 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é posspivel excluir uma categoria que possui produto"); 
 		}		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
