c0872d96fff405d933a9fc966a4c8a597011f448
leandrowakim <leandrowakim@gmail.com>
2021-05-18 16:32:49 -0300

    Subtotal de pedido itens e Total do pedido.

--- src/main/java/com/lwinfo/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/lwinfo/cursomc/repositories/ProdutoRepository.java
@@ -15,8 +15,7 @@ import com.lwinfo.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
-	@Transactional(readOnly = true)
+	@Transactional(readOnly = true)		 
 	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
-	Page<Produto> findDistincByNomeContainigAndCatagoriaIN(@Param("nome") String nome,@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	Page<Produto> findDistincByNomeContainigAndCatagoriaIN(@Param("nome") String nome,@Param("categorias") List<Categoria> categorias, Pageable pageRequest);	 	 
 }
