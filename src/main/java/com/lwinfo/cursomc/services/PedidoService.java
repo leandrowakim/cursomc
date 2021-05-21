7b67bfbe2698f28e332d622d0a02e38e400984b0
leandrowakim <leandrowakim@gmail.com>
2021-05-21 11:55:07 -0300

    Email HTML

--- src/main/java/com/lwinfo/cursomc/services/PedidoService.java
+++ src/main/java/com/lwinfo/cursomc/services/PedidoService.java
@@ -69,7 +69,7 @@ public class PedidoService {
 		}
 		itemPedidoRepo.saveAll(obj.getItens());
 		//System.out.println(obj);		
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 }
